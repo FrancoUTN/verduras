cfc8c2c9b208a5313b7af6999ff6692b8a3b3269
unknown <cataniafrancoar@gmail.com>
2021-06-14 02:46:23 -0300

    de mysql local a remoto

--- app/index.php
+++ app/index.php
@@ -32,28 +32,28 @@ $app->addRoutingMiddleware();
 $container=$app->getContainer();
 
 $capsule = new Capsule;
-// $capsule->addConnection([
-//     'driver'    => 'mysql',
-//     'host'      => $_ENV['MYSQL_HOST'],
-//     'database'  => $_ENV['MYSQL_DB'],
-//     'username'  => $_ENV['MYSQL_USER'],
-//     'password'  => $_ENV['MYSQL_PASS'],
-//     'charset'   => 'utf8',
-//     'collation' => 'utf8_unicode_ci',
-//     'prefix'    => '',
-// ]);
 $capsule->addConnection([
     'driver'    => 'mysql',
-
-    'host'      => 'localhost',
-    'database'  => 'simulacro',
-    'username'  => 'root',
-    'password'  => '',
-    
+    'host'      => $_ENV['MYSQL_HOST'],
+    'database'  => $_ENV['MYSQL_DB'],
+    'username'  => $_ENV['MYSQL_USER'],
+    'password'  => $_ENV['MYSQL_PASS'],
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
 ]);
+// $capsule->addConnection([
+//     'driver'    => 'mysql',
+
+//     'host'      => 'localhost',
+//     'database'  => 'simulacro',
+//     'username'  => 'root',
+//     'password'  => '',
+    
+//     'charset'   => 'utf8',
+//     'collation' => 'utf8_unicode_ci',
+//     'prefix'    => '',
+// ]);
 
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
